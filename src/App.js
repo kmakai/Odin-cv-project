bd1121f09fbc6573da2abfc5b1c2b934a57c24a5
kmakai <92133709+kmakai@users.noreply.github.com>
2022-09-12 19:28:40 -0400

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import './styles/Appstyle.css';
-import {PersonInfo, PersonInfoEdit} from './components/PersonInfo.js';
+import { PersonInfo, PersonInfoEdit } from './components/PersonInfo.js';
 
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      info: {
+        name: 'John Doe',
+        title: 'Designer',
+        phone: '860-555-5555',
+        email: 'John.doe@example.com',
+      },
+    };
   }
 
   render() {
+    const {info} = this.state;
     return (
       <div>
         <h1></h1>
-        <PersonInfo info={{ name: 'John Doe', title: "Designer", phone: "860-555-5555", email: "John.doe@example.com" }} />
+        <PersonInfo
+          info={info}
+        />
         <PersonInfoEdit />
       </div>
     );
